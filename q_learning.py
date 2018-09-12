cb14a76023942bce555e7647f8600855ebd4b0c2
beddingearly <wangzitongnowb@163.com>
2018-09-12 18:44:30 +0800

    0912-q_learning

--- q_learning.py
+++ q_learning.py
@@ -31,15 +31,79 @@ def build_q_table(n_states, actions):
 # 在某个 state 地点, 选择行为
 def choose_action(state, q_table):
     state_actions = q_table.iloc[state, :]  # 选出这个 state 的所有 action 值
-    print state_actions
+    #print state_actions
     if (np.random.uniform() > EPSILON) or (state_actions.all() == 0):  # 非贪婪 or 或者这个 state 还没有探索过
         action_name = np.random.choice(ACTIONS)
     else:
         action_name = state_actions.argmax()    # 贪婪模式
-    print action_name
+    #print action_name
     return action_name
+
+def get_env_feedback(S, A):
+    # This is how agent will interact with the environment
+    if A == 'right':    # move right
+        if S == N_STATES - 2:   # terminate
+            S_ = 'terminal'
+            R = 1 # 成功一个给个reward
+        else:
+            S_ = S + 1
+            R = 0
+    else:   # move left
+        R = 0
+        if S == 0:
+            S_ = S  # reach the wall
+        else:
+            S_ = S - 1
+    return S_, R
+
+def update_env(S, episode, step_counter):
+    # This is how environment be updated
+    env_list = ['-']*(N_STATES-1) + ['T']   # '---------T' our environment
+    if S == 'terminal':
+        interaction = 'Episode %s: total_steps = %s' % (episode+1, step_counter)
+        print('\r{}'.format(interaction), '')
+        time.sleep(2)
+        print('\r                                ', '')
+    else:
+        env_list[S] = 'o'
+        interaction = ''.join(env_list)
+        print('\r{}'.format(interaction), '')
+        time.sleep(FRESH_TIME)
+
+
+def rl():
+    q_table = build_q_table(N_STATES, ACTIONS)  # 初始 q table
+    for episode in range(MAX_EPISODES):     # 回合
+        step_counter = 0
+        S = 0   # 回合初始位置
+        is_terminated = False   # 是否回合结束
+        update_env(S, episode, step_counter)    # 环境更新
+        while not is_terminated:
+            A = choose_action(S, q_table)   # 选行为 S = 0, S位置索引
+            print('A', A)
+            S_, R = get_env_feedback(S, A)  # 实施行为并得到环境的反馈 A = 'left'
+            print('S_', S_, 'R', R)
+            q_predict = q_table.loc[S, A]    # 估算的(状态-行为)值
+            print('q_predict', q_predict)
+            if S_ != 'terminal':
+                q_target = R + GAMMA * q_table.iloc[S_, :].max()   #  实际的(状态-行为)值 (回合没结束)
+            else:
+                q_target = R     #  实际的(状态-行为)值 (回合结束)
+                is_terminated = True    # terminate this episode
+            print('q_target', q_target)
+            #  q_table 更新
+            q_table.loc[S, A] += ALPHA * (q_target - q_predict)  # （现实-估计） * 学习效率
+            print('S', S)
+            S = S_  # 探索者移动到下一个 state
+            print('S', S)
+            print(q_table)
+            update_env(S, episode, step_counter+1)  # 环境更新
+
+            step_counter += 1
+    return q_table
 
 
 if __name__ == '__main__':
-    q_table = build_q_table(N_STATES, ACTIONS)
-    choose_action(0, q_table)
+    q_table = rl()
+    print('\r\nQ-table:\n')
+    print(q_table)
